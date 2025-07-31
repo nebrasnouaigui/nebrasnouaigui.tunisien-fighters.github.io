999fd5a7644e84012140a9a6cee25623125382b4
nebrasnouaigui <nebrasse.nouaigui@gmail.com>
2025-08-01 01:37:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -17,19 +17,18 @@ const config = {
   }
 };
 
-let player1, player2, cursors, keys, attackKey1, attackKey2, healthBar1, healthBar2;
+let player1, player2, cursors, keys, attackKey1;
 let hp1 = 100, hp2 = 100;
+let healthBar1, healthBar2;
+let touchControls = { left: false, right: false, jump: false, attack: false };
 
 const game = new Phaser.Game(config);
 
-function preload() {
-  this.load.image('bg', 'https://i.imgur.com/YOUR_BACKGROUND_IMAGE.png'); // Placeholder
-}
+function preload() {}
 
 function create() {
   this.add.rectangle(400, 300, 800, 600, 0x2e2e2e); // Background
 
-  // Players (colored blocks)
   player1 = this.physics.add.sprite(200, 450, null).setDisplaySize(50, 100).setTint(0xff0000);
   player2 = this.physics.add.sprite(600, 450, null).setDisplaySize(50, 100).setTint(0x0000ff);
 
@@ -38,59 +37,47 @@ function create() {
 
   cursors = this.input.keyboard.createCursorKeys();
   keys = this.input.keyboard.addKeys('Q,D,Z,A,L');
-
   attackKey1 = keys.A;
-  attackKey2 = keys.L;
 
-  // Ground
   const ground = this.add.rectangle(400, 580, 800, 40, 0x666666);
   this.physics.add.existing(ground, true);
   this.physics.add.collider(player1, ground);
   this.physics.add.collider(player2, ground);
 
-  // Health bars
   healthBar1 = this.add.rectangle(150, 30, hp1 * 2, 20, 0xff0000);
   healthBar2 = this.add.rectangle(650, 30, hp2 * 2, 20, 0x0000ff);
+
+  setupTouchControls();
 }
 
 function update() {
-  // Player 1 controls (Q, D, Z)
-  if (keys.Q.isDown) {
+  if (keys.Q.isDown || touchControls.left) {
     player1.setVelocityX(-160);
-  } else if (keys.D.isDown) {
+  } else if (keys.D.isDown || touchControls.right) {
     player1.setVelocityX(160);
   } else {
     player1.setVelocityX(0);
   }
-  if (keys.Z.isDown && player1.body.touching.down) {
+  if ((keys.Z.isDown || touchControls.jump) && player1.body.touching.down) {
     player1.setVelocityY(-350);
   }
 
-  // Player 2 controls (←, →, ↑)
-  if (cursors.left.isDown) {
-    player2.setVelocityX(-160);
-  } else if (cursors.right.isDown) {
-    player2.setVelocityX(160);
-  } else {
-    player2.setVelocityX(0);
-  }
-  if (cursors.up.isDown && player2.body.touching.down) {
-    player2.setVelocityY(-350);
-  }
-
-  // Attacks
-  if (Phaser.Input.Keyboard.JustDown(attackKey1)) {
+  if (Phaser.Input.Keyboard.JustDown(attackKey1) || touchControls.attack) {
     if (Phaser.Math.Distance.Between(player1.x, player1.y, player2.x, player2.y) < 100) {
       hp2 -= 10;
       healthBar2.width = hp2 * 2;
       healthBar2.x = 650 - (100 - hp2);
     }
+    touchControls.attack = false;
   }
-  if (Phaser.Input.Keyboard.JustDown(attackKey2)) {
-    if (Phaser.Math.Distance.Between(player2.x, player2.y, player1.x, player1.y) < 100) {
-      hp1 -= 10;
-      healthBar1.width = hp1 * 2;
-      healthBar1.x = 150 - (100 - hp1);
-    }
-  }
+}
+
+function setupTouchControls() {
+  document.getElementById("left").addEventListener("touchstart", () => touchControls.left = true);
+  document.getElementById("left").addEventListener("touchend", () => touchControls.left = false);
+  document.getElementById("right").addEventListener("touchstart", () => touchControls.right = true);
+  document.getElementById("right").addEventListener("touchend", () => touchControls.right = false);
+  document.getElementById("jump").addEventListener("touchstart", () => touchControls.jump = true);
+  document.getElementById("jump").addEventListener("touchend", () => touchControls.jump = false);
+  document.getElementById("attack").addEventListener("touchstart", () => touchControls.attack = true);
 }
